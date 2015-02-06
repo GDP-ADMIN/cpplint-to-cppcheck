1f61cf7532a540c0a4e79864a469a41cd16a4649
Roy Inganta Ginting <roy.i.ginting@gdplabs.id>
2015-02-06 14:01:21 +0700

    Adjust output to cppcheck version 2

--- cpplint-to-cppcheckxml.py
+++ cpplint-to-cppcheckxml.py
@@ -13,21 +13,23 @@ import xml.sax.saxutils
 def cpplint_score_to_cppcheck_severity(score):
     # I'm making this up
     if score == 1:
-        return 'cpplint style'
+        return 'style'
     elif score == 2:
-        return 'cpplint style'
+        return 'style'
     elif score == 3:
-        return 'cpplint warning'
+        return 'warning'
     elif score == 4:
-        return 'cpplint warning'
+        return 'warning'
     elif score == 5:
-        return 'cpplint error'
+        return 'error'
 
 def parse():
     # TODO: do this properly, using the xml module.
     # Write header
     sys.stderr.write('''<?xml version="1.0" encoding="UTF-8"?>\n''')
-    sys.stderr.write('''<results>\n''')
+    sys.stderr.write('''<results version="2">\n''')
+    sys.stderr.write('''  <cppcheck version=""/>\n''')
+    sys.stderr.write('''  <errors>\n''')
 
     pattern="%{DATA:fname}:%{INT:lineno}: %{GREEDYDATA:rawmsg} \[%{DATA:label}\] \[%{INT:severity}"
 
@@ -40,9 +42,12 @@ def parse():
         # Protect Jenkins from bad XML, which makes it barf
         msg = xml.sax.saxutils.quoteattr(m['rawmsg'])
         severity = cpplint_score_to_cppcheck_severity(int(m['severity']))
-        sys.stderr.write('''<error file="%s" line="%s" id="%s" severity="%s" msg=%s/>\n'''%(m['fname'], m['lineno'], m['label'], severity, msg))
+        sys.stderr.write('''  <error id="%s" severity="%s" msg=%s>\n'''%(m['label'], severity, msg))
+        sys.stderr.write('''    <location file="%s" line="%s"/>\n'''%(m['fname'], m['lineno']))
+        sys.stderr.write('''  </error>\n''')
 
     # Write footer
+    sys.stderr.write('''  </errors>\n''')
     sys.stderr.write('''</results>\n''')
 
 if __name__ == '__main__':
